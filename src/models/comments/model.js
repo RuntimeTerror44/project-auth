d4a5fe0a64ca0db202720cd7186c543d7fe0d63f
Mohammad-Aljamal <mohammad.o.aljamal@gmail.com>
2023-06-26 09:32:24 +0000

    comments-table DONE

--- src/models/comments/model.js
+++ src/models/comments/model.js
@@ -1,9 +1,16 @@
-'use strict';
+"use strict";
 
-const foodModel = (sequelize, DataTypes) => sequelize.define('Food', {
-  name: { type: DataTypes.STRING, required: true },
-  calories: { type: DataTypes.INTEGER, required: true },
-  type: { type: DataTypes.ENUM('fruit', 'vegetable', 'protein'), required: true }
-});
+const commentsModel = (sequelize, DataTypes) =>
+  sequelize.define("comments", {
+    content: { type: DataTypes.STRING, required: true },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  });
 
-module.exports = foodModel;
+module.exports = commentsModel;
